828b1f3aa02e03424e92d5cc33d7ec54ae67e5d2
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-09-18 00:43:39 +0700

    add realm

--- src/screens/AddNoteScreen.js
+++ src/screens/AddNoteScreen.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
+import realm from '../../store/realm';
+
+const saveNote = (newNote) => {
+    const allData = realm.objects('Note');
+    const dataLength = allData.length;
+    let lastIdFromRealm = 0;
+
+    if (dataLength !== 0) {
+        lastIdFromRealm = allData[dataLength - 1].id;
+    }
+
+    if (newNote !== '') {
+        realm.write(() => {
+            realm.create("Note", {
+                id: dataLength === 0 ? 1 : lastIdFromRealm + 1,
+                note: newNote,
+                date: new Date().toISOString()
+            });
+        });
+        alert('Successfully save your note!');
+        const data = realm.objects('Note');
+        console.log(data);
+    } else {
+        alert('Empty note!');
+    }
+};
 
 const AddNoteScreen = (props) => {
     const { navigation } = props;
+    const [tempNote, setTempNote] = useState('');
+
     return (
         <View style={styles.mainContainer}>
             <View style={styles.headerContainer}>
                 <Text style={styles.title}>Create</Text>
                 <TouchableOpacity
                     style={styles.button}
+                    onPress={() => saveNote(tempNote)}
                 >
                     <Icon
                         name="check"
@@ -23,6 +52,7 @@ const AddNoteScreen = (props) => {
                 multiline
                 placeholder="Write here"
                 style={styles.input}
+                onChangeText={(text) => setTempNote(text)}
             />
         </View>
     )
@@ -30,33 +60,33 @@ const AddNoteScreen = (props) => {
 
 const styles = StyleSheet.create({
     mainContainer: {
-        flex:1
+        flex: 1
     },
     headerContainer: {
-        padding:8,
-        backgroundColor:'moccasin',
-        alignItems:'center',
-        flexDirection:'row',
-        justifyContent:'space-between'
+        padding: 8,
+        backgroundColor: 'moccasin',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     title: {
-        fontSize:20,
-        padding:8,
-        fontWeight:'bold'
+        fontSize: 20,
+        padding: 8,
+        fontWeight: 'bold'
     },
     button: {
-        padding:8
+        padding: 8
     },
     date: {
-        paddingTop:16,
-        paddingLeft:16
+        paddingTop: 16,
+        paddingLeft: 16
     },
     input: {
-        fontSize:16,
-        flex:1,
-        paddingRight:16,
-        paddingLeft:16,
-        textAlignVertical:'top'
+        fontSize: 16,
+        flex: 1,
+        paddingRight: 16,
+        paddingLeft: 16,
+        textAlignVertical: 'top'
     }
 });
 
