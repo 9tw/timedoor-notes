be5e51312594a1b30503398ae851003406b2bf7a
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-10-01 16:27:10 +0900

    adding save edited note

--- src/screens/EditNoteScreen.js
+++ src/screens/EditNoteScreen.js
@@ -7,6 +7,43 @@ const EditNoteScreen = (props) => {
     const { route, navigation } = props;
     const id = route.params.id;
     const [dataToUpdate, setDataToUpdate] = useState([]);
+    const [newNote, setNewNote] = useState('');
+    const [isEdit, setIsEdit] = useState(false);
+
+    const dateFormat = (date) => {
+        const months = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+        const noteDate = new Date(date);
+        const dateOnly = noteDate.getDate();
+        const monthOnly = noteDate.getMonth();
+        const yearOnly = noteDate.getFullYear();
+        return months[monthOnly] + ' ' + dateOnly + ', ' + yearOnly;
+    };
+
+    const saveNote = (value) => {
+        if (value === '') {
+            alert(`Note can't be empty!`);
+        } else {
+            const allData = realm.objects('Note');
+            allData.forEach((item) => {
+                if (item.id === id && item.note !== value) {
+                    realm.write(() => {
+                        item.note = value;
+                        item.date = new Date().toISOString();
+                    });
+                    navigation.navigate('NoteList');
+                } else if (item.id === id &&
+                    item.note === value) {
+                    alert('Nothing changed!');
+                }
+            });
+        }
+    };
+
+    const editNote = (value, editStatus) => {
+        setNewNote(value);
+        setIsEdit(editStatus);
+    };
 
     useEffect(() => {
         console.log('edit screen');
@@ -22,10 +59,20 @@ const EditNoteScreen = (props) => {
         <View style={styles.mainContainer}>
             <HeaderComponent
                 title="Edit"
+                onPress={() => saveNote(
+                    isEdit ? newNote : dataToUpdate[0].note
+                )}
             />
-            <MainComponent
-                date="Date"
-            />
+            {
+                dataToUpdate.length !== 0 ?
+                    <MainComponent
+                        date={dateFormat(dataToUpdate[0].date)}
+                        value={isEdit ? newNote : dataToUpdate[0].note}
+                        onChangeText={(text) => editNote(text, true)}
+                    />
+                    :
+                    null
+            }
         </View>
     )
 }
